8f054b0e546b9dd37f42d597752f9d1445bd3cab
USER\Michał <michal.rudzky@gmail.com>
2019-04-09 21:30:21 +0200

    Initial commit

---
+++ ProjZes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjZes.Startup))]
+namespace ProjZes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
